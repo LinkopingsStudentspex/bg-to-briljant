a4de2a8eb65c5108aa0343827bdb1da4c9066f6a
Rovanion Luckey <rovanion.luckey@gmail.com>
2019-02-02 11:09:58 +0100

    Added a file of small utilities.

---
+++ src/bg_to_briljant/utilities.clj
@@ -0,0 +1,9 @@
+(ns bg-to-briljant.utilities
+  (:require [clojure.string :as string]))
+
+(defn capitalize-words
+  "Capitalize every word in a string"
+  [s]
+  (->> (string/split (str s) #"\b")
+       (map string/capitalize)
+       string/join))
